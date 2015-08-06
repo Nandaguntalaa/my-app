5da3e8ac5cf164210965841d7e80e3663147015d
Nandaguntalaa <Nandaguntalaa@users.noreply.github.com>
2015-08-06 08:34:10 -0700

    Update App.java

--- src/main/java/com/test/myapp/App.java
+++ src/main/java/com/test/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World this is to me!" );
+        System.out.println( "Hello World this is to me!" )
     }
 }
